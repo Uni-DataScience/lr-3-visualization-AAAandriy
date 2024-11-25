4023669756e926a4480250acb8483e858dc9488e
AAAandriy <156540030+AAAandriy@users.noreply.github.com>
2024-11-26 00:50:05 +0200

    Update task1_visualization.py

--- src/task1_visualization.py
+++ src/task1_visualization.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import collections
 
-
 def plot_distribution(data):
     """
     Plots the distribution of data using a bar chart.
@@ -10,12 +9,21 @@ def plot_distribution(data):
     Parameters:
     data (array-like): An array of categorical data items.
     """
+    counter = collections.Counter(data)
     
-    fig, _ = plt.subplots()
-    pass
+    categories = list(counter.keys())
+    frequencies = list(counter.values())
+
+    fig, ax = plt.subplots()
+    ax.bar(categories, frequencies, color=['skyblue', 'lightgreen', 'lightcoral'])
+
+    ax.set_xlabel('Category')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Distribution of Categories')
+
+    plt.show()
     return fig
 
-
-# Example data
 data = np.random.choice(['A', 'B', 'C'], size=100)
+
 plot_distribution(data)
